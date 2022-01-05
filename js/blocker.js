2cd6171b5504f1806f80d372df38f9486039e4b2
jfinlayson78 <johnfinlayson78@gmail.com>
2022-01-04 18:20:01 -0700

    Experimenting with notifications.

--- js/blocker.js
+++ js/blocker.js
@@ -1,8 +1,9 @@
-let redirectURL = 'cnn.com';
-const blockedURLs = ["redscarepod", "stupidpol", "askanamerican", "moderatepolitics", "politicalcompassmemes"]; 
+let redirectURL = 'https://www.github.com';
+const blockedURLs = ["redscarepod", "stupidpol", "askanamerican", "moderatepolitics", "politicalcompassmemes", "actualpublicfreakouts", "combatfootage"]; 
 
 // A listener that checks when a new URL is entered
 chrome.tabs.onUpdated.addListener( function (tabID, changeInfo, tab) {
+    console.log("Checking URL: " + changeInfo.url)
     if (changeInfo.url) {
         blockedURLs.forEach(redirectUser, changeInfo.url);
     };
@@ -10,8 +11,9 @@ chrome.tabs.onUpdated.addListener( function (tabID, changeInfo, tab) {
 
 // if a blocked URL string is detected, redirect to the chosen site
 function redirectUser(item, index, arr) {
-    console.log(this);
     if (this.toLowerCase().includes(item)) {
+        console.log("found a match...")
         chrome.tabs.update(undefined, {url : redirectURL});
     };
+    //chrome.notifications.create("Hello World!")
 };
